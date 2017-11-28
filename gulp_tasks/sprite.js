16f1a31b98c66250356e7d8fd95f985f2c136288
Ryosuke Taniguchi <cre-taniguchi@zyyx.jp>
2017-11-28 15:14:09 +0900

    ファイル分割

---
+++ gulp_tasks/sprite.js
@@ -0,0 +1,68 @@
+const gulp = require('gulp');
+const spritesmith = require('gulp.spritesmith');
+const imageResize = require('gulp-image-resize');
+
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
+
+const rimraf = require('rimraf');
+const rename = require('gulp-rename');
+
+const paths = {
+  sprite: {
+    src: 'src/sprite',
+    dest: './dist/assets/sprite',
+    tmp: 'tmp/sprite',
+  },
+};
+
+module.exports = () => {
+  gulp.task('sprite.clean', (cb) => {
+    rimraf(paths.sprite.tmp, cb);
+  });
+
+  gulp.task('sprite.resize', ['sprite.clean'], () => {
+    return gulp.src(paths.sprite.src + '/**/*.png')
+      .pipe(imageResize({
+        percentage: 50,
+        imageMagick: true,
+      }))
+      .pipe(gulp.dest(paths.sprite.tmp));
+  });
+
+  gulp.task('sprite.copy', ['sprite.resize'], () => {
+    return gulp.src(paths.sprite.src + '/**/*.png')
+      .pipe(rename({
+        suffix: '@2x',
+      }))
+      .pipe(gulp.dest(paths.sprite.tmp));
+  });
+
+  gulp.task('sprite.sprite', ['sprite.copy'], () => {
+    return gulp.src(paths.sprite.tmp + '/**/*.png')
+      .pipe(spritesmith({
+        imgName: 'sprite.png',
+        cssName: 'sprite.css',
+        retinaImgName: 'sprite@2x.png',
+        retinaSrcFilter: [paths.sprite.tmp + '/**/*@2x.png'],
+      }))
+      .pipe(gulp.dest(paths.sprite.dest));
+  });
+
+  gulp.task('sprite.compress', ['sprite.sprite'], () => {
+    return gulp.src(paths.sprite.dest + '/**/*.png')
+      .pipe(imagemin({
+        progressive: true,
+        use: [
+          pngquant({
+            quality: '60-80'
+          })
+        ]
+      }))
+      .pipe(gulp.dest(paths.sprite.dest));
+  });
+
+  gulp.task('sprite', () => {
+    gulp.start('sprite.compress');
+  });
+};
